0a4a05f3409a81a1f503119862302e81848a780f
mango <elig7393@gmail.com>
2019-03-16 12:34:38 -0500

    update

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -6,12 +6,12 @@ in the export pass parameter app -Express.. when user selects url, deliver surve
 module.exports = function (app) {
 
     app.get("/survey", function(req, res) {
-        res.sendFile(path.join(__dirname + "../public/survey.html"));
+        res.sendFile(path.join(__dirname + "/../public/survey.html"));
 
     });
 
     app.use(function(req, res) {
-        res.sendFile(path.join(__dirname + "../public/home.html"));
+        res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
     
 
